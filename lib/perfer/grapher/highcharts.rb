07a3c12b8aa5a06797887175a49c540f1f4f58ae
Benoit Daloze <eregontp@gmail.com>
2014-05-04 12:00:05 +0200

    simplify handling of categories

--- lib/perfer/grapher/highcharts.rb
+++ lib/perfer/grapher/highcharts.rb
@@ -14,26 +14,20 @@ module Perfer
                     .order(:job, :ruby)
                     .to_hash_groups(:job, [:ruby, :s_per_iter])
 
-      categories = {}
+      categories = []
       times_per_job.each_pair do |job, times|
-        times.each do |ruby, s_per_iter|
-          unless categories.key? ruby
-            categories[ruby] = categories.size
-          end
+        times.each do |ruby, _|
+          categories << ruby unless categories.include? ruby
         end
       end
 
-      series = []
-      times_per_job.each_pair { |job, times|
-        d = categories.map { 0.0 }
-        times.each { |ruby, s_per_iter|
-          d[categories[ruby]] = s_per_iter
-        }
-        series << { name: job, data: d }
+      series = times_per_job.map { |job, times|
+        times = Hash[times].tap { |h| h.default = 0.0 }
+        { name: job, data: times.values_at(*categories) }
       }
 
       title = session.name
-      categories = categories.keys.map { |ruby| Formatter.short_ruby_description(ruby) }
+      categories.map! { |ruby| Formatter.short_ruby_description(ruby) }
 
       require 'erb'
       puts ERB.new((Path.dir/'highcharts'/'barplot.html.erb').read).result(binding)

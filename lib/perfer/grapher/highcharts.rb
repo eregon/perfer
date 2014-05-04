296fef6a51b06ea6dc01bcda1592dcd69639aef4
Benoit Daloze <eregontp@gmail.com>
2014-05-04 15:53:46 +0200

    refactor HighchartsGrapher

--- lib/perfer/grapher/highcharts.rb
+++ lib/perfer/grapher/highcharts.rb
@@ -5,6 +5,23 @@ module Perfer
       require 'erb'
     end
 
+    def compute_unit(time_per_x)
+      min_scale = 0
+      time_per_x.each_pair { |x, times|
+        times.each { |_,t|
+          scale = Formatter.float_scale(t)
+          min_scale = scale if scale < min_scale
+        }
+      }
+      unit = Formatter::TIME_UNITS[min_scale]
+      in_units = 10 ** (-min_scale)
+      [unit, in_units]
+    end
+
+    def render_template(name, b)
+      ERB.new((Path.dir/'highcharts'/"#{name}.html.erb").read).result(b)
+    end
+
     def barplot(session)
       db = session.store.db
       file = session.file.to_s
@@ -22,10 +39,7 @@ module Perfer
         end
       end
 
-      min_scale = times_per_job.inject(0) { |min, (_, times)|
-        [min, times.map { |_,t| Formatter.float_scale(t) }.min].min
-      }
-      in_units = 10 ** (-min_scale)
+      unit, in_units = compute_unit(times_per_job)
 
       series = times_per_job.map { |job, times|
         times = Hash[times].tap { |h| h.default = 0.0 }
@@ -34,10 +48,9 @@ module Perfer
       }
 
       title = session.name
-      unit = Formatter::TIME_UNITS[min_scale]
       categories.map! { |ruby| Formatter.short_ruby_description(ruby) }
 
-      puts ERB.new((Path.dir/'highcharts'/'barplot.html.erb').read).result(binding)
+      puts render_template('barplot', binding)
     end
 
     def timelines(session)
@@ -53,19 +66,15 @@ module Perfer
                      .order(:ruby, :run_time)
                      .to_hash_groups(:ruby, [:run_time, :s_per_iter])
 
-      min_scale = times_per_ruby.inject(0) { |min, (_, times)|
-        [min, times.map { |_,t| Formatter.float_scale(t) }.min].min
-      }
-      in_units = 10 ** (-min_scale)
+      unit, in_units = compute_unit(times_per_ruby)
 
       series = times_per_ruby.map { |ruby,times|
-        d = times.map { |run_time,t| [run_time.strftime('%Q').to_i, (t * in_units).round(1)] }
-        { name: Formatter.short_ruby_description(ruby), data: d }
+        times.map! { |run_time,t| [run_time.strftime('%Q').to_i, (t * in_units).round(1)] }
+        { name: Formatter.short_ruby_description(ruby), data: times }
       }
       title = session.name
-      unit = Formatter::TIME_UNITS[min_scale]
 
-      puts ERB.new((Path.dir/'highcharts'/'timelines.html.erb').read).result(binding)
+      puts render_template('timelines', binding)
     end
   end
 end
